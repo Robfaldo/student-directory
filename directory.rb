96f6fbcb662cb358ed5383304812b1758c338623
Robert Faldo <robertfaldo@gmail.com>
2018-05-07 14:00:54 +0100

    completed exercise 7

--- directory.rb
+++ directory.rb
@@ -1,47 +1,69 @@
-# First we print the list of students
-
-# create a student class, with variables for name, age, height, nationality 
-# until the user quits, prompt the user to create 
-# prompt the user for input and assign it to
-# use that input to set the variables of the instance of student 
-# return the 
-
-
+# Create a student class with variables 
 class Student 
-  def initialize(name) #, cohort, height, nationality)
+  attr_accessor :age, :height, :nationality
+
+  def initialize(name, cohort) 
     @name = name
-    #@cohort = cohort.to_sym
-    #@height = height
-    #@nationality = nationality
+    @cohort = cohort 
   end 
 
   def return_name 
     return @name.capitalize 
   end
 
-  def age(age)
-    @age = age
+  def return_cohort 
+    return @cohort.downcase
   end
+end
 
-  def return_age
-    return @age
-  end
+def is_valid_cohort(cohort)
+  # If the cohort value is a month then return true, else return false 
+  # @type: string
+  # @rtype bool 
+  valid_months = [:january, :february, :march, :april, :may, :june, :july, :august, 
+  :september, :october, :november, :december]
+
+  return valid_months.include?(cohort)
+
 end
 
 
 def create_new_student
 
   puts "Enter student name: "
-  user_input = gets.chomp.capitalize
+  name = gets.chomp.capitalize
 
-  if user_input.empty? 
+  if name.empty? 
     return "stop"
-  else
-    student = Student.new(user_input)
   end
 
+  puts "What cohort is #{name} in?"
+  cohort = gets.chomp.downcase.to_sym
+
+
+  # check cohort is valid and set to default if not & prompt user if they'd like to try again
+  until is_valid_cohort(cohort)
+    cohort = :november
+    puts "No matching cohort, default set to november. Are you sure you'd like to submit (type yes/no)?"
+    wants_to_submit_anyway = gets.chomp.downcase 
+
+    if wants_to_submit_anyway == "yes"
+      break
+    else
+      cohort = gets.chomp.downcase.to_sym
+    end
+  end 
+
+  student = Student.new(name, cohort)
+
   puts "How old is #{student.return_name}? "
-  student.age(gets.chomp)
+  student.age = gets.chomp
+
+  puts "How tall is #{student.return_name} in cm's?"
+  student.height = gets.chomp
+
+  puts "What nationality is #{student.return_name}?"
+  student.nationality = gets.chomp
 
   return student
 end
@@ -54,22 +76,18 @@ def input_students
   # array of hash tables to store each student. 
   students = []
 
-
+  # unless user stops entering names, prompt the user to create a new student and store 
+  # the instance of the student class in an array of students 
   while true
     current_student = create_new_student
 
     if current_student == "stop"
       break
     else
-      students << { name: current_student.return_name, age: current_student.return_age }
+      students << { name: current_student.return_name, age: current_student.age, height: current_student.height, nationality: current_student.nationality, cohort: current_student.return_cohort }
     end
   end
 
-  puts "Enter student age" 
-
-
-
-  p students
   return students 
 end
 
@@ -91,13 +109,15 @@ def print_footer(names)
   while name_index < names.length
     
     if names[name_index][:name].length < 12
-      puts "#{i}. #{names[name_index][:name]} who is #{names[name_index][:age]} years old!"
+      list = "#{i}. #{names[name_index][:name]} who is #{names[name_index][:age]} years old! Hes in the #{names[name_index][:cohort]} cohort"
+      puts list.center(15)
       i += 1
     end
     name_index += 1
   end
 
 end
+
 
 current_student_directory = input_students
 print_header
