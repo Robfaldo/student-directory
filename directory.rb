001e9d502d80f80f955a4aeedd9b4d6fdca247c5
Robert Faldo <robertfaldo@gmail.com>
2018-05-07 08:47:04 +0100

    Added user input to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,17 +1,23 @@
 # First we print the list of students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+
+def input_students 
+  students = []
+  puts "Enter new student name, to finish hit return twice: "
+
+  while true 
+    new_name = gets.chomp.capitalize
+
+    if new_name.empty?
+      break
+    else
+      students << {name: new_name, cohort: :november} 
+    end
+  end
+
+  p students
+  return students 
+end
+
 
 def print_header
   puts "The students of Villains Academy"
@@ -28,7 +34,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+current_student_directory = input_students
 print_header
-print(students)
-print_footer(students)
+print_footer(current_student_directory)
 
