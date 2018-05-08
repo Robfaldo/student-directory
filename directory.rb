5204d518fb0722b9f6e6e4a7b90bac90b0691838
Robert Faldo <robertfaldo@gmail.com>
2018-05-08 12:40:54 +0100

    completed 10 and refactored code, good code not great though

--- directory.rb
+++ directory.rb
@@ -127,11 +127,46 @@ def print_footer(names)
       end
     end
   end
-
+  puts "-------------------"
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
-current_student_directory = input_students
-print_header
-print_footer(current_student_directory)
+def show_students(students) 
+  print_header
+  print_footer(students)
+end
+
+def interactive_menu
+  students = []
+
+  loop do 
+    # 1. print the menu and ask the user what to do. Add student, 
+    print_menu
+
+    # 2. read the input and save it into a variable
+    user_input = gets.chomp.downcase
+
+    # 3. do what the user has asked
+    case user_input
+    when "1"
+      students = input_students
+    when "2"
+      show_students(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
+end
+
+interactive_menu
+# current_student_directory = input_students
+#print_header
+#print_footer(current_student_directory)
 
